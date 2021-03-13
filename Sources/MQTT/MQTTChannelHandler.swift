c57f262f60a6f3c7ca9c4b03282adad4b3b130e0
Tom Knapen <tom@knapen.io>
2021-03-13 07:56:40 +0100

    convert indentation to spaces

--- Sources/MQTT/MQTTChannelHandler.swift
+++ Sources/MQTT/MQTTChannelHandler.swift
@@ -13,45 +13,45 @@ final class MQTTChannelHandler: ChannelInboundHandler {
 
     private let decoder: MQTTDecoder
     weak var delegate: MQTTChannelHandlerDelegate?
-	private var packetData = Data()
+    private var packetData = Data()
 
     init(decoder: MQTTDecoder = MQTTDecoder()) {
         self.decoder = decoder
     }
 
     func channelRead(context: ChannelHandlerContext, data: NIOAny) {
-		defer { context.fireChannelRead(data) }
+        defer { context.fireChannelRead(data) }
 
         var buf = unwrapInboundIn(data)
         if let bytes = buf.readBytes(length: buf.readableBytes) {
-			packetData.append(contentsOf: bytes)
+            packetData.append(contentsOf: bytes)
         }
     }
 
-	func channelReadComplete(context: ChannelHandlerContext) {
-		defer { context.fireChannelReadComplete() }
+    func channelReadComplete(context: ChannelHandlerContext) {
+        defer { context.fireChannelReadComplete() }
 
-		var data = packetData
-		packetData = Data()
+        var data = packetData
+        packetData = Data()
 
-		do {
-			let packet = try decoder.decode(data: &data)
-			delegate?.didReceive(packet: packet)
-		} catch let error as DecodeError {
-			delegate?.didCatch(decodeError: error)
-		} catch {
-			// unhandled error
-			fatalError("Error while decoding packet data: \(error.localizedDescription)")
-		}
-	}
+        do {
+            let packet = try decoder.decode(data: &data)
+            delegate?.didReceive(packet: packet)
+        } catch let error as DecodeError {
+            delegate?.didCatch(decodeError: error)
+        } catch {
+            // unhandled error
+            fatalError("Error while decoding packet data: \(error.localizedDescription)")
+        }
+    }
 
     func channelActive(context: ChannelHandlerContext) {
-		defer { context.fireChannelActive() }
+        defer { context.fireChannelActive() }
         delegate?.channelActive(channel: context.channel)
     }
 
     func channelInactive(context: ChannelHandlerContext) {
-		defer { context.fireChannelInactive() }
+        defer { context.fireChannelInactive() }
         delegate?.channelInactive()
     }
 }
